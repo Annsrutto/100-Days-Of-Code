11928d5f0d1eb97c032dd9a837b1d2d97afcb222
Annsrutto <annsrutto567@gmail.com>
2024-07-25 02:40:49 +0300

    variables practice

---
+++ Day_02-JavaScript/variables-practise/index.js
@@ -0,0 +1,15 @@
+// let firstName = "Anns"
+// let lastName = "Rutto"
+
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName);
+
+let name = "Linda"
+let greeting = "Hi there"
+
+function greetingName() {
+    console.log(greeting + ", " + name + "!");
+}
+
+greetingName();
