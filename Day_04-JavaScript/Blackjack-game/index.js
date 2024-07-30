1ebeb037363e37f66dc99f0d37c13c80ecafe98f
Annsrutto <annsrutto567@gmail.com>
2024-07-30 03:54:08 +0300

    Blackjack game

--- Day_04-JavaScript/Blackjack-game/index.js
+++ Day_04-JavaScript/Blackjack-game/index.js
@@ -1,13 +1,38 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 10;
 
-let sum = firstCard + secondCard + 6;
+let sum = firstCard + secondCard;
 console.log(sum);
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw another card? 🙂")
 } else if (sum === 21) {
     console.log("Whohoo! You've got a Blackjack! 🥳")
 } else {
     console.log("You're out of the game! 😭")
 }
+
+
+
+
+
+
+
+
+// let age = 20;
+
+// if (age < 21) {
+//     console.log("You cannot enter the club!")
+// } else {
+//     console.log("Welcome!")
+// }
+
+// let age = 102;
+
+// if (age < 100) {
+//     console.log('Not eligible!')
+// } else if (age === 100) {
+//     console.log('Here is your birthday card from the King!')
+// } else {
+//     console.log('Not eligible, you have already gotten one!')
+// }
