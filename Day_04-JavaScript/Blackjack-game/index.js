59842f56b7112cc2526a8aa81b4920c98c175234
Annsrutto <annsrutto567@gmail.com>
2024-07-31 01:12:26 +0300

    Blackjack game

--- Day_04-JavaScript/Blackjack-game/index.js
+++ Day_04-JavaScript/Blackjack-game/index.js
@@ -1,25 +1,30 @@
 let firstCard = 10;
-let secondCard = 1;
-
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
-console.log(sum);
 
 let message = '';
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+//let sumEl = document.querySelector('#sum-el');
 
+function startGame() {
+    sumEl.textContent = "Sum: " + sum;
 
-if (sum <= 20) {
-    message = "Do you want to draw another card? 🙂"
-} else if (sum === 21) {
-    message = "Whohoo! You've got a Blackjack! 🥳"
-    hasBlackjack = true;
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false;
+    if (sum <= 20) {
+        message = "Do you want to draw another card?"
+    } else if (sum === 21) {
+        message = "You've got a Blackjack!"
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!"
+        isAlive = false;
+    }
+    messageEl.textContent = message
 }
 
-console.log(hasBlackjack);
-console.log(isAlive);
+// console.log(hasBlackjack);
+// console.log(isAlive);
 
-console.log(message);
+// console.log(message);
