e24bba55a5c50d6b9c3b2e2b62679cd09d375f1a
Annsrutto <annsrutto567@gmail.com>
2024-07-31 03:50:41 +0300

    Blackjack game

--- Day_04-JavaScript/Blackjack-game/index.js
+++ Day_04-JavaScript/Blackjack-game/index.js
@@ -1,5 +1,6 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
@@ -10,12 +11,20 @@ let sumEl = document.getElementById("sum-el");
 //let sumEl = document.querySelector('#sum-el');
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+    return 5;
+}
+
 function startGame() {
     renderGame();
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+        console.log(cards)
+    }
     sumEl.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
@@ -32,8 +41,10 @@ function renderGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!");
-    let card = 4;
+    let card = getRandomCard();
     sum += card;
+    cards.push(card);
+    console.log(cards)
     renderGame();
 }
 
@@ -41,3 +52,33 @@ function newCard() {
 // console.log(isAlive);
 
 // console.log(message);
+
+
+
+
+//Arrays in JavaScript
+
+// let aboutMe = ["Anns", 26, true]
+// let newMessage = "I am a software Engineer"
+
+//Push() and Pop() function 
+// aboutMe.push(newMessage)
+// console.log(aboutMe)
+// aboutMe.pop();
+// console.log(aboutMe)
+// aboutMe.pop();
+// console.log(aboutMe)
+
+// For Loops
+// let cards = [9, 1, 8]
+// for (let i = 0; i < cards.length; i++) {
+//     console.log(cards[i])
+// }
+
+// let sentence = ["Hello", "my", "name", "is", "per"]
+// let greetingEl = document.getElementById("greeting-el")
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i] + " "
+//     //console.log(sentence[i])
+// }
