8138568a4ce62cc0d80f0227c7a523e8b2b3e38d
Annsrutto <annsrutto567@gmail.com>
2024-08-07 16:29:38 +0300

    objects

---
+++ Day_04-JavaScript/practise-time/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: 'Anns',
+    age: 26,
+    country: 'Kenya'
+}
+console.log(person.country)
+
+function logData() {
+    console.log(person.name + ' is ' + person.age + ' years old and lives in ' + person.country)
+}
+logData()
